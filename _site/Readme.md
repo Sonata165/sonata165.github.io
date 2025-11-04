2f8f0351563b69c5b4dd7de5eca65dc630202755
Sonata165 <oulongshen@outlook.com>
2025-11-04 17:06:37 +0800

    clear unnecessary files

--- _site/Readme.md
+++
@@ -1,1 +0,0 @@
-This is Longshen Ou's personal website, powered by Jekyll.
--- _site/jekyll_serve.sh
+++
@@ -1,4 +0,0 @@
-# cd into Longshen's personal website, serve locally with jekyll
-# cd Code/PersonalSite/github_page/
-jekyll serve
-
